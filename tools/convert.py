79ee6e57d418a6724174de84a05fa6ab3834f599
SHooZ <SHooZ@ex.ua>
2018-06-16 19:11:00 +0300

    Fix converting to txt;

--- tools/convert.py
+++ tools/convert.py
@@ -1,9 +1,11 @@
+#!/usr/bin/env python
+
 from openpyxl import Workbook, load_workbook
 from sys import argv
 
 
 def to_xlsx(name: str) -> None:
-    print(f'Working with {name}.txt...')
+    print(f'Working with {name}.txt…')
     wb = Workbook()
     ws = wb.active
 
@@ -11,7 +13,7 @@ def to_xlsx(name: str) -> None:
         content = file.readlines()
 
     def parce_lines(lines, prefix):
-        return [t[1:] for t in lines
+        return [t[1:-1] for t in lines
                 if t.startswith(prefix) and len(t)]
 
     keys = parce_lines(content, '#')
@@ -24,22 +26,27 @@ def to_xlsx(name: str) -> None:
         ws[f'B{i+1}'] = string
 
     wb.save(f'{name}.xlsx')
-    print(f'Converted to {name}.xlsx !')
+    print(f'Converted to {name}.xlsx!')
 
 
 def to_txt(name: str) -> None:
-    print(f'Working with {name}.xlsx...')
+    print(f'Working with {name}.xlsx…')
     wb = load_workbook(f'{name}.xlsx')
     ws = wb.active
 
-    translation = [(row[0].value, row[1].value) for row in ws]
+    # if there are more than 2 columns -- use third one, else -- second one
+    trans_index = 1 if len(ws[1]) == 2 else 2
+
+    translation = [(row[0].value, row[trans_index].value) for row in ws]
 
     with open(f'{name}.txt', 'w', encoding='utf-16') as file:
         for key, string in translation:
+            if {key}.startswith('Identification (key)'):
+                continue
             file.write(f'#{key}\n')
             file.write(f'={string}\n\n')
 
-    print(f'Converted to {name}.txt !')
+    print(f'Converted to {name}.txt!')
 
 
 def main(argv):
@@ -55,4 +62,4 @@ def main(argv):
 
 if __name__ == '__main__':
     main(argv)
-    input('Press ENTER to close the window...')
+    input('Press ENTER to close the window…')
